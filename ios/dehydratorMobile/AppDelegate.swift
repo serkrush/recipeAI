d1b3278f9f4302c169d603cf706c541b115cd748
Serhii Krushelnitskyi <serhiikrushelnitskyi@MacBook-Air-Serhii.local>
2025-02-21 21:43:36 +0200

    start ios

---
+++ ios/dehydratorMobile/AppDelegate.swift
@@ -0,0 +1,28 @@
+import UIKit
+import React
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    let jsCodeLocation: URL
+
+    #if DEBUG
+      jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    #else
+      jsCodeLocation = Bundle.main.url(forResource: "main", withExtension: "jsbundle")!
+    #endif
+
+    let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "dehydratorMobile", initialProperties: nil, launchOptions: launchOptions)
+    rootView.backgroundColor = UIColor.white
+
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+    self.window?.rootViewController = rootViewController
+    self.window?.makeKeyAndVisible()
+
+    return true
+  }
+}
